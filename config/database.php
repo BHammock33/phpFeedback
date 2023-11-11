5228661371b210d0b1dfb81b1a0e7d3579229720
BHammock33 <BennettHammock@gmail.com>
2023-11-11 17:02:46 -0500

    connected DB, moved files, configured port

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+    ini_set("mysqli.default_port", 3307);
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'Bennett');
+    define('DB_PASS', '123456');
+    define('DB_NAME', 'php_dev');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($conn->connect_error){
+    die('Connection failed ' . $conn->connect_error);
+}
+
+
+echo 'Connected!';
